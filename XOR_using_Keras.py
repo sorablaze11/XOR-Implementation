559b9f21951cbbcbf13e19ebeac0cf5f1eff76e9
Sukh Raj Limbu <limburaj65@gmail.com>
2017-07-26 21:48:48 +0530

    Update XOR_using_Keras.py

--- XOR_using_Keras.py
+++ XOR_using_Keras.py
@@ -1,13 +1,29 @@
+'''This is a simple implementation of XOR Neural Network using keras.
+    Accuracy - 100%
+    Author - sorablaze_11
+'''
+
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 
+# Creating the model for our Neural Network
 model = Sequential()
+
+#Adding first hidden layer with 2 input parameters, 3 neurons and setting up the sigmoid activation function.
 model.add(Dense(3, activation = 'sigmoid', input_dim = 2))
+
+#Final layer takes input and produces output
 model.add(Dense(1, activation = 'sigmoid'))
+
+# Setting up our cost function, optimizer function
 model.compile(optimizer = 'rmsprop', loss = 'mse', metrics = ['accuracy'])
+
+# Our datasets
 data = np.array(((1, 1), (1, 0), (0, 1), (0, 0)))
 labels = np.array((0, 1, 1, 0))
+
+#Training our model
 model.fit(data, labels, epochs = 5000)
 print('Enter two numbers:')
 x, y = map(int, input().split())
